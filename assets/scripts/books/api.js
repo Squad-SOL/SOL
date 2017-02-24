c06715592151153288999347f7d8e83d275e6dd7
dsweetser <daniel.sweetser@gmail.com>
2017-02-24 11:17:00 -0500

    create books directory create API call for books

---
+++ assets/scripts/books/api.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const getBooks = function () {
+  return $.ajax({
+    url: `https://wdi-library.herokuapp.com/books/`,
+    method: 'GET',
+  });
+};
+
+module.exports = {
+  getBooks
+};
